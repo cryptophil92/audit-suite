772d59743c5369655e2214d78a0cd44136c077b7
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-01 00:00:34 +0200

    fix(shellcheck): annotate service module runtime env

--- modules/40_service_enum.sh
+++ modules/40_service_enum.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # modules/40_service_enum.sh
-# @version 0.2.0
+# @version 0.2.1
+# shellcheck disable=SC2154
 set -Eeuo pipefail
 MOD_ID="40_service_enum"
 MOD_NAME="Énumération services"
