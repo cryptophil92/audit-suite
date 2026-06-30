3412a0efa25cdd3c73a61e675361fe36c421e6f1
cryptophil92 <54324136+cryptophil92@users.noreply.github.com>
2026-07-01 00:13:55 +0200

    fix(shellcheck): include report metadata/runtime suppressions

--- modules/90_report_pack.sh
+++ modules/90_report_pack.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # modules/90_report_pack.sh
-# @version 0.2.1
-# shellcheck disable=SC2154
+# @version 0.2.2
+# shellcheck disable=SC2034,SC2153,SC2154
 set -Eeuo pipefail
 MOD_ID="90_report_pack"
 MOD_NAME="Pack rapport"
